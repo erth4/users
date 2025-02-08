2cc7c12ecae6ff9e4b2a282b98fbc65bf59d5942
Ertha Dwi Setiyawan <86835067+erth4@users.noreply.github.com>
2025-02-08 21:52:45 +0700

    baru

---
+++ precache-manifest.17bc3013f2016955afb562fd8c64c905.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ac8ada36c3bfcc99f4e",
+    "url": "/users/css/app.cf3a6cef.css"
+  },
+  {
+    "revision": "67313e1e5464649217d8",
+    "url": "/users/css/chunk-vendors.000176dc.css"
+  },
+  {
+    "revision": "cc75c57917caa2cc9fa42881be1f4f4b",
+    "url": "/users/index.html"
+  },
+  {
+    "revision": "4ac8ada36c3bfcc99f4e",
+    "url": "/users/js/app.8e62343c.js"
+  },
+  {
+    "revision": "67313e1e5464649217d8",
+    "url": "/users/js/chunk-vendors.1f69018f.js"
+  },
+  {
+    "revision": "729caba64bc7ad81d83fd8cb2a6a705c",
+    "url": "/users/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/users/robots.txt"
+  }
+]);
