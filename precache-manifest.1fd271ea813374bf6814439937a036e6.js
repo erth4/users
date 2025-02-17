e389ae6eee89d782513611970309928b89b331af
Ertha Dwi Setiyawan <86835067+erth4@users.noreply.github.com>
2025-02-17 12:45:53 +0700

    Add files via upload

---
+++ precache-manifest.1fd271ea813374bf6814439937a036e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69b05d29036a80aba2df",
+    "url": "/users/css/app.cf3a6cef.css"
+  },
+  {
+    "revision": "67313e1e5464649217d8",
+    "url": "/users/css/chunk-vendors.000176dc.css"
+  },
+  {
+    "revision": "4727e4b596773efb8bfb8603286d34a7",
+    "url": "/users/index.html"
+  },
+  {
+    "revision": "69b05d29036a80aba2df",
+    "url": "/users/js/app.d05c597e.js"
+  },
+  {
+    "revision": "67313e1e5464649217d8",
+    "url": "/users/js/chunk-vendors.1f69018f.js"
+  },
+  {
+    "revision": "729caba64bc7ad81d83fd8cb2a6a705c",
+    "url": "/users/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/users/robots.txt"
+  }
+]);
